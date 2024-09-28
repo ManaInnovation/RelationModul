3cc9c438597333a80e2f39a4dd81236b76580f92
FatemehAbouei <fatemeh.ab0098@gmail.com>
2024-09-28 16:55:51 +0330

    ok

--- V1/Main.py
+++ V1/Main.py
@@ -4,6 +4,7 @@ import os
 import logging
 import EntityBehave
 
+
 app = Flask(__name__)
 
 #logging.basicConfig(level=logging.DEBUG)
@@ -16,19 +17,20 @@ app = Flask(__name__)
 @app.route('/run-program')
 # @app.route('/run-program', methods=['POST'])
 def run_program():
-    # #program_env_python = 'C:/Users/win11/miniconda3/envs/myprogramenv/python.exe'
-    # program_env_python = 'C:/Users/win11/AppData/Local/Microsoft/WindowsApps/python3.exe'
-    # #program_env_python = "/home/ubuntu/miniconda3/envs/myprogramenv/bin/python"
+   
 
    
-    # program_script_path = 'D:/projects/Git_projects/RelationModul/mini_flask_project/ver3/EntityBehave.py'
-    
-    # result = subprocess.run([program_env_python, program_script_path], capture_output=True, text=True)
-     relation_matrix = EntityBehave.V2RelationMatrix()
-     relation_matrix.StartProcess()
+     try:
+          relation_matrix = EntityBehave.V2RelationMatrix()
+          relation_matrix.StartProcess()
 
-     myjson = relation_matrix.Array3d
-     return myjson
+          myjson = relation_matrix.Array3d
+          return myjson
+          
+     except:
+          return '{"result":"error occured"}'
+          
+     
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
