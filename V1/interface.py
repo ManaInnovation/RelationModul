9b8121f5f2a1b2c9f1c0054f11d495a3de0a7b72
FatemehAbouei <fatemeh.ab0098@gmail.com>
2024-09-30 14:28:17 +0330

    ok

---
+++ V1/interface.py
@@ -0,0 +1,15 @@
+import requests
+
+url = 'http://13.38.16.203:6000/run-command'
+data = {'command': 'ls -l'}
+response = requests.post(url, json=data)
+
+# Print the status code and response text
+print("Status Code:", response.status_code)
+print("Response Text:", response.text)
+
+# If the response is successful, decode the JSON
+if response.ok:
+    print(response.json())
+else:
+    print("Failed to get a valid JSON response.")
